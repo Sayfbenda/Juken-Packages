e5aa75007ef19054a93ceba457a991bc07c5b3ab
Sayfbenda <soufiane.bakouri.dev@gmail.com>
2025-10-23 13:32:29 +0200

    Add Player Spawn manager

---
+++ juken-gamemode/Server/Index.lua
@@ -0,0 +1,11 @@
+
+
+function SpawnPlayer(player)
+    local character = Character(Vector(200, 0, 100), Rotator(0, 0, 0), "nanos-world::SK_Mannequin")
+    player:Posses(character)
+    
+end
+
+Player.Subscribe("Spawn", function (player)
+    SpawnPlayer(player)
+end)
