a9a312f7a7334bceb4f3945de5c3f8234d35c491
Sayfbenda <soufiane.bakouri.dev@gmail.com>
2025-10-23 13:24:51 +0200

    Main Hud visible by default

--- juken-gamemode/Client/Mainhud.lua
+++ juken-gamemode/Client/Mainhud.lua
@@ -1,9 +1,5 @@
 local mainhud = WebUI(
     "ScoreBard",
     "file://UI/mainhud/index.html",
-    WidgetVisibility.Hidden
+    WidgetVisibility.Visible
 )
-
-Input.Bind("Mainhud", InputEvent.Pressed, function()
-    Console.Log("Hello")
-end)
