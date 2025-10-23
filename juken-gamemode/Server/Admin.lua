6ef2318d5d4b424c0772ec49977f7b4f5d31f113
Sayfbenda <soufiane.bakouri.dev@gmail.com>
2025-10-23 14:10:18 +0200

    Test fix

--- juken-gamemode/Server/Admin.lua
+++ juken-gamemode/Server/Admin.lua
@@ -1,10 +1,4 @@
 Events.BroadcastRemote("ToggleNoclip", function (player)
     local character = player:GetControlledCharacter()
-    if (not character) then return end
-
-    if character:GetFlyingMode() == true then
-        character:SetFlyingMode(false)
-    else
         character:SetFlyingMode(true)
-    end
 end)
