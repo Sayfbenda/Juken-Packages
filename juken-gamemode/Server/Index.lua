c4970fe95e0db311aed7230db1e58f8769d36402
Sayfbenda <soufiane.bakouri.dev@gmail.com>
2025-10-23 13:34:09 +0200

    Fix player spawn

--- juken-gamemode/Server/Index.lua
+++ juken-gamemode/Server/Index.lua
@@ -2,7 +2,7 @@
 
 function SpawnPlayer(player)
     local character = Character(Vector(200, 0, 100), Rotator(0, 0, 0), "nanos-world::SK_Mannequin")
-    player:Posses(character)
+    player:Possess(character)
     
 end
 
