15e887dfc6ca3d7011e790cd614b975b2cfa9a74
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-24 20:14:23 +0200

    Update Rotation on Client tick

--- juken-gamemode/Client/Mainhud.lua
+++ juken-gamemode/Client/Mainhud.lua
@@ -8,13 +8,12 @@ function UpdateHealth(health)
     mainhud:CallEvent("UpdateHealth", health)
 end
 
-
-Events.Subscribe("Update", function ()
+function UpdateCardinalBar()
     local player = Client.GetLocalPlayer()
     if (not player) then
         return
     end
-    
+
     local character = player:GetControlledCharacter()
     if (not character) then
         return
@@ -22,4 +21,6 @@ Events.Subscribe("Update", function ()
     local rotator = character:GetControlRotation()
     local yaw_value = string.match(tostring(rotator), "Yaw%s*=%s*([%d%.%-]+)")
     mainhud:CallEvent("UpdateCardinalBar", yaw_value)
-end)
+end
+
+Client.Subscribe("Tick", UpdateCardinalBar)
