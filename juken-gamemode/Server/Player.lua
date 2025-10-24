7e82cba2c79738c1307484ca5e47e59f15677586
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-24 21:25:27 +0200

    add energy to all players

--- juken-gamemode/Server/Player.lua
+++ juken-gamemode/Server/Player.lua
@@ -1,6 +1,17 @@
-function SpawnPlayer(player)
+function PlayerManager(player)
+    SpawnPlayer(player)
+end
+
+function SpawnPlayer(player, health, energy)
     local character = Character(Vector(200, 0, 100), Rotator(0, 0, 0), "nanos-world::SK_Mannequin")
+    character:SetMaxHealth(health)
+    character:SetValue("energymax", energy, true)
     player:Possess(character)
+end
+
+function GetPlayerValues()
+    local health = HOKAGE.hpmax
+    local energy = HOKAGE.energymax
 end
 
 function DestroyPlayer(player)
