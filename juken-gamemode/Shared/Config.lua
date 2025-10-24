63840810046a66b8a3dcd9910d7559f327e8c5a9
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-24 21:20:51 +0200

    add grades

--- juken-gamemode/Shared/Config.lua
+++ juken-gamemode/Shared/Config.lua
@@ -1,4 +1,52 @@
+GENIN = {
+    hpmax = 500;
+    energymax = 250
+}
+GENINCONFIRME = {
+    hpmax = 750;
+    energymax = 500
+}
+CHUNIN = {
+    hpmax = 1000;
+    energymax = 750
+}
+CHUNINCONFIRME = {
+    hpmax = 1500;
+    energymax = 900
+}
+KAKUNIN = {
+    hpmax = 1750;
+    energymax = 1400
+}
+TOKUBETSU = {
+    hpmax = 2000;
+    energymax = 1750
+}
+JONIN = {
+    hpmax = 2500;
+    energymax = 1900
+}
+COMMANDANTJONIN = {
+    hpmax = 3000;
+    energymax = 2300
+}
+HOKAGE = {
+    hpmax = 4500;
+    energymax = 3000
+}
+SANIN = {
+    hpmax = 4400;
+    energymax = 3300
+}
+
 GRADES = {
-    "Sayf",
-    "Le GOAT"
+    GENIN;
+    GENINCONFIRME;
+    CHUNIN;
+    CHUNINCONFIRME;
+    TOKUBETSU;
+    JONIN;
+    COMMANDANTJONIN;
+    HOKAGE;
+    SANIN
 }
