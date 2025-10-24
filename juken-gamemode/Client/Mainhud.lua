d3d6b329120db9120afa00ad7b34be07530f431b
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-24 19:56:07 +0200

    Canvas to events

--- juken-gamemode/Client/Mainhud.lua
+++ juken-gamemode/Client/Mainhud.lua
@@ -9,16 +9,12 @@ function UpdateHealth(health)
 end
 
 
-
-local my_canvas = Canvas(
-  true,
-  Color.TRANSPARENT,
-  0,
-  true
-)
-
-my_canvas:Subscribe("Update", function(self, width, height)
+Events.Subscribe("Update", function ()
     local player = Client.GetLocalPlayer()
+    if (not player) then
+        return
+    end
+    
     local character = player:GetControlledCharacter()
     if (not character) then
         return
@@ -26,9 +22,4 @@ my_canvas:Subscribe("Update", function(self, width, height)
     local rotator = character:GetControlRotation()
     local yaw_value = string.match(tostring(rotator), "Yaw%s*=%s*([%d%.%-]+)")
     mainhud:CallEvent("UpdateCardinalBar", yaw_value)
-    self:DrawText(yaw_value, Vector2D(width / 2, height / 2))
-end)
-
-mainhud:Subscribe("Update", function ()
-    Console.Log("test")
 end)
