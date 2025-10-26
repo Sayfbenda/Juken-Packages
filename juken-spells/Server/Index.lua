b2ec3d744d52a03e4af8381256f884bc25a76e2f
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-26 16:42:41 +0100

    Stable version

--- juken-spells/Server/Index.lua
+++ juken-spells/Server/Index.lua
@@ -2,21 +2,31 @@ Package.Require("Config.lua")
 
 local woman = Character(Vector(100, 0, 100), Rotator(0, 0, 0), "nanos-world::SK_Female")
 
+local test = Particle(Vector(), Rotator(), "jknassets::NS_PrisonGlace")
+
+
 Events.SubscribeRemote("LunchSpell", function (player, character, playerrotation)
     Console.Log(character)
-    local characterLocation = character:GetLocation()
-    local characterRotation = character:GetRotation()
-    SpawnSpell(character:GetLocation(), character:GetRotation(), playerrotation)
+    local owner = player:GetName()
+    SpawnSpell(owner, character:GetLocation(), character:GetRotation(), playerrotation)
 end)
 
-function SpawnSpell(characterLocation, characterRotation, playerrotation)
+function SpawnSpell(owner ,characterLocation, characterRotation, playerrotation)
     local spellprop = Prop(characterLocation, characterRotation, TEST.proppath, CollisionType.StaticOnly)
+    spellprop:SetValue("owner", owner)
     local hitBox = Trigger(spellprop:GetLocation(), spellprop:GetRotation(), TEST.size, TriggerType.Sphere, true, Color.RED, {"Character"})
     hitBox:AttachTo(spellprop)
+    hitBox:Subscribe("BeginOverlap", function (self, entity)
+        Console.Log("Nathan")
+    end)
+    spellprop:Subscribe("Destroy", function ()
+        
+    end)
     AddImpulseToSpell(spellprop, characterLocation, characterRotation, playerrotation)
 end
 
 function AddImpulseToSpell(spellprop, characterLocation, characterRotation, playerrotation)
     local playerRotationForward = playerrotation:GetForwardVector()
-    spellprop:AddImpulse(characterLocation+playerRotationForward*300000)
+    spellprop:AddImpulse(characterLocation+playerRotationForward*100000)
 end
+
