7961fc8fb44de554625521477e87484d4c740747
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-27 16:38:37 +0100

    Remove old gamemode

--- juken-gamemode/Server/Admin.lua
+++
@@ -1,9 +0,0 @@
-Events.SubscribeRemote("ToggleNoclip", function (player)
-    local character = player:GetControlledCharacter()
-    if (not character) then return end
-    if character:GetFlyingMode() == true then
-        character:SetFlyingMode(false)
-    else
-        character:SetFlyingMode(true)
-    end
-end)
