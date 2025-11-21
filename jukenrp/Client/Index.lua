e721e2b192fe171d601daf01fd5693fa5f991777
Sayf <soufiane.bakouri.dev@gmail.com>
2025-11-21 18:56:48 +0100

    Toggle Mouse

--- jukenrp/Client/Index.lua
+++ jukenrp/Client/Index.lua
@@ -1,1 +1,24 @@
 Package.Require("CharacterCreator.lua")
+
+MainHUD = WebUI(
+    "MainHud",
+    "file://UI//index.html",
+    WidgetVisibility.Visible
+)
+
+Input.Register("MouseToggle", "C")
+
+Input.Bind("MouseToggle", InputEvent.Pressed, function ()
+    MouseToggle(Input.IsMouseEnabled())
+end)
+
+function MouseToggle(isToggle)
+    if isToggle then
+        Input.SetMouseEnabled(false)
+    else
+        Input.SetMouseEnabled(true)
+    end
+end
+
+
+
